b1212a1b8cf97a84daefa46370966456feadfd7b
blakerutledge <blake@blakerutledge.com>
2016-01-27 00:38:52 -0500

    resume link, and silenced console logs. soft launch

--- front-page.php
+++ front-page.php
@@ -36,7 +36,7 @@
 			<div class="projectsWrap">
 				<div class="info-container">
 					<p class="info-emphasis">I am a Brooklyn-based interactive & motion designer.</p>
-					<p class="info-more">I work as an Artist in Residence at Ogilvy & Mather. I'm passionate about concepting and designing for film and web, hybrid processes, and the outdoors. Here's <a target="_blank" href="<? echo home_url(); ?>/wp-content/uploads/2016/01/BlakeRutledge_resume_011016.pdf">my resume,</a> and if you'd like to get in touch, send me <a class="info-email-fakeout" >an email,</a> I read 'em all.</p>
+					<p class="info-more">I work as an Artist in Residence at Ogilvy & Mather. I'm passionate about concepting and designing for film and web, hybrid processes, and the outdoors. Here's <a target="_blank" href="http://blakerutledge.com/wp/wp-content/uploads/2016/01/BlakeRutledge_Resume_012616.pdf">my resume,</a> and if you'd like to get in touch, send me <a class="info-email-fakeout" >an email,</a> I read 'em all.</p>
 				</div>
 				<div class="info-nav">
 					<a href="projects" class="info-button">
